556cb165a62e91cc785e4095b82174372aa1f913
Tai Kao-Sowa <taikaosowa11@gmail.com>
2020-05-25 20:38:54 -0700

    a bit more code

--- testGPIO.py
+++ testGPIO.py
@@ -10,31 +10,39 @@ import time
 
 DIRPIN =16 
 STEPPIN = 18
-STEPSPERREVOLUTION = 400 # test
-
+STEPSPERREVOLUTION = 800
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(DIRPIN, GPIO.OUT)
     GPIO.setup(STEPPIN, GPIO.OUT)
 
-def one_revolution_clockwise():
 
-    # Set direction clockwise
-    GPIO.output(DIRPIN, GPIO.HIGH)
-    
-    for i in range(STEPSPERREVOLUTION):
+def step(numsteps: int, time: float):
+    for i in range(numsteps):
         GPIO.output(STEPPIN, GPIO.HIGH)
-        time.sleep(.01)
+        time.sleep(time)
         GPIO.output(STEPPIN, GPIO.LOW)
-        time.sleep(.01)
+        time.sleep(time)
+
+
+def one_revolution_clockwise():
+    GPIO.output(DIRPIN, GPIO.HIGH)  # Set direction clockwise
+    step(STEPSPERREVOLUTION, .005)
+
+
+def one_revolution_counterclockwise():
+    GPIO.output(DIRPIN, GPIO.LOW)  # Set direction clockwise
+    step(STEPSPERREVOLUTION, .005)
 
 
 if __name__ == '__main__':
     print('Setting up...')
     setup()
-    print('Running test code...')
-    one_revolution()
+    print('One clockwise rotation')
+    one_revolution_clockwise()
+    print('One counterclockwise rotation')
+    one_revolution_clockwise()
     print('Finished!')
     GPIO.cleanup()
 
